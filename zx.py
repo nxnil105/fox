8dc35e5a47425cbec410b37fa20ea8a5fb41cf0e
nxnil105 <113341774+nxnil105@users.noreply.github.com>
2022-11-12 16:35:18 +0600

    Create zx.py

---
+++ zx.py
@@ -0,0 +1,60 @@
+import os, platform
+
+ 
+
+try:
+
+ 
+
+        import requests
+
+ 
+
+except:
+
+ 
+
+        os.system('pip2 install requests')
+
+ 
+
+ 
+
+ 
+
+import requests
+
+ 
+
+bit = platform.architecture()[0]
+
+ 
+
+if bit == "64bit":
+
+ 
+
+        from zx import login
+
+ 
+
+        login()
+
+ 
+
+ 
+
+ 
+
+elif bit == "32bit":
+
+ 
+
+        from zx import login
+
+ 
+
+ 
+
+        login()
+
